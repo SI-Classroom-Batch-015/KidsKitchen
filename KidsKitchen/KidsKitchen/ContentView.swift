a7190707c19a839200c8a3f40be0ace7450d2328
JoschaSyntax <155442435+JoschaAmaniGaber@users.noreply.github.com>
2024-09-07 03:37:06 +0200

    Add: - ReadMe

---
+++ KidsKitchen/KidsKitchen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KidsKitchen
+//
+//  Created by Joscha Amani Gaber on 06.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KidsKitchen/KidsKitchen/KidsKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsKitchenApp.swift
+//  KidsKitchen
+//
+//  Created by Joscha Amani Gaber on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct KidsKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
